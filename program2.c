79a184ef880d856563f9c78c38d71ddab8e7101a
shelkeom230 <shelkeom230@gmail.com>
2023-06-13 19:07:37 +0530

    starting with c programming after very long time

---
+++ program2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(){
+    int a;
+    int b;
+    printf("enter a number");
+    scanf("%d",&a);
+    printf("eneter number 2");
+    scanf("%d",&b);
+    int sum=a+b;    
+}
